6c7c29ed4ee0b1941cc3667e14075b3d131abf2f
coinbringer <cjones@kei-coin.com>
2024-02-03 13:12:46 -0500

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import reportWebVitals from "./reportWebVitals";
+
+// Use the non-null assertion operator (!) to assert that the element is not null.
+const rootElement = document.getElementById("root")!;
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
